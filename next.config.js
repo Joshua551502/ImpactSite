b016dcab7802cb82d57f8c03e9dfb025401e1669
Joshua Moore <mojoshua@sheridancollege.ca>
2025-03-26 11:34:54 -0400

    Final Adjustments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-};
-
-module.exports = nextConfig;
+    output: 'export',
+    // Optional: Add other config options here if needed
+  };
+  
+  export default nextConfig;
+  
