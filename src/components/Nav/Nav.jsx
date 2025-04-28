4551bc3073e4ea3f32df3f85c39f702b276a921a
Joshua Moore <mojoshua@sheridancollege.ca>
2025-04-27 20:03:14 -0400

    First Mobile Edits

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Nav.module.css";
 
 export default function Nav({ scrollPercent }) {
-  const maxScrollbarTrackHeight = 150 - 20; // container height - thumb height
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  // Auto close menu if screen resized larger than 800px
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 800 && menuOpen) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [menuOpen]);
+
+  const maxScrollbarTrackHeight = 150 - 20;
   const scrollbarPosition = `${
     (scrollPercent / 100) * maxScrollbarTrackHeight
   }px`;
@@ -10,6 +28,15 @@ export default function Nav({ scrollPercent }) {
   return (
     <>
       <nav className={styles.nav}>
+        <div
+          className={`${styles.hamburger} ${menuOpen ? styles.open : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+
         <ul className={styles.menu} id={styles.menuLeft}>
           <li className={styles.menuItem} id={styles.mission} role="button">
             MISSION
@@ -39,13 +66,23 @@ export default function Nav({ scrollPercent }) {
           </li>
         </ul>
       </nav>
-      <div className={styles.sidebar} id={styles.leftSideBar}>
-        <div className={styles.scrollbarContainer}>
-        
-        </div>
-       
+
+      <div
+        className={`${styles.mobileMenu} ${
+          menuOpen ? styles.open : styles.closed
+        }`}
+      >
+        <button className={styles.mobileMenuItem}>MISSION</button>
+        <button className={styles.mobileMenuItem}>INVEST</button>
+        <button className={styles.mobileMenuItem}>WEB3</button>
+        <button className={styles.mobileMenuItem}>PRIVACY</button>
+        <button className={styles.mobileMenuItem}>MERCH</button>
+        <button className={styles.mobileMenuItem}>DISCORD</button>
+        <button className={styles.mobileMenuItem}>ACCOUNT</button>
+        <button className={styles.mobileMenuItem}>LIGHTPAPER</button>
       </div>
 
+      <div className={styles.sidebar} id={styles.leftSideBar}></div>
       <div className={styles.sidebar} id={styles.rightSideBar}></div>
     </>
   );
